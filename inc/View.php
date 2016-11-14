3b72a4f8866547f15fc66387301214484635c9f1
docker <dock3r@gmail.com>
2016-11-14 16:36:43 +0100

    Add view context

---
+++ inc/View.php
@@ -0,0 +1,37 @@
+<?php
+namespace cncPP;
+
+class View {
+	private $data = array();
+
+	public function __construct()
+	{
+	}
+
+	/**
+	 * Assign new value to data stack
+	 * @param  string $variable Variable name
+	 * @param  mixed $value    Value of the variable
+	 */
+	public function assign($variable, $value)
+	{
+		$this->data[$variable] = $value;
+	}
+
+	/**
+	 * Render content using the given template file
+	 * @param  string $template Template file name without extension
+	 * @return string           Generated HTML markup
+	 */
+	public function render($template)
+	{
+		extract($this->data);
+		$file = CNC_TEMPLATE_DIR . CNC_DS . $template . '.tpl.php';
+		if (!file_exists($file)) {
+			throw new \Exception("File doesn't exist");
+		}
+		ob_start();
+		include($file);
+		return ob_get_clean();
+	}
+}
---
+++ templates/popup-content.tpl.php
@@ -0,0 +1,4 @@
+<h2><?php echo $post_title; ?></h2>
+<div class="cnc-popup-content">
+	<?php echo $post_content; ?>
+</div>
