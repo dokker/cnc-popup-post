c225a757683bb6c975ed55944f8405be1ba2a911
docker <dock3r@gmail.com>
2016-11-14 16:20:26 +0100

    Prepare scripts for use of AJAX

--- inc/Controller.php
+++ inc/Controller.php
@@ -3,8 +3,12 @@ namespace cncPP;
 
 class Controller {
 	public function __construct() {
+		$this->plugin_path = plugin_dir_path(dirname(__FILE__));
+		$this->plugin_url = plugin_dir_url(dirname(__FILE__));
+
 		add_action('wp_ajax_get_post_content', [$this, 'ajax_get_post_content']);
 		add_action('wp_ajax_nopriv_get_post_content', [$this, 'ajax_get_post_content']);
+		add_action('wp_enqueue_scripts', [$this, 'registerScripts']);
 	}
 
 	/**
@@ -17,5 +21,21 @@ class Controller {
 		$post_content = '';
 		wp_send_json($post_content);
 		wp_die();
+	}
+
+	/**
+	 * Callback for register necessary scripts
+	 * @return [type] [description]
+	 */
+	public function registerScripts()
+	{
+		wp_register_script('cnc-popup-post-main', $this->plugin_url . 'assets/js/main.js', array('jquery'), '1', true);
+		// Prepare script for use AJAX
+		wp_localize_script( 'cnc-popup-post-main', 'cnc_popup_post_obj', array(
+			'ajax_url' => admin_url( 'admin-ajax.php' ),
+		    'nonce'    => wp_create_nonce('cncpp_nonce'),
+	    ) );
+		// Builtin WP function to register thickbox dependent scripts
+	    add_thickbox();
 	}
 }
